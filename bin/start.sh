ec28f189b0c73f2ed3946648a32be15a9480b8f2
U-fusion\jim <jim@fusion.(none)>
2012-05-02 03:21:10 -0700

    cygwin

--- bin/start.sh
+++ bin/start.sh
@@ -3,16 +3,20 @@
 JIT_LOGGING_OPT='-XX:+AggressiveOpts -XX:+AlwaysCompileLoopMethods -XX:+BackgroundCompilation -XX:+CMSClassUnloadingEnabled -XX:-DontCompileHugeMethods -XX:MaxHeapSize=134217728 -XX:+PrintCommandLineFlags -XX:+PrintCompilation -XX:+PrintGC -XX:+PrintVMOptions -XX:+RelaxAccessControlCheck'
 
 rt=$PWD/$(dirname $0 )
-p=$(grep '<artifactId>' $rt/../pom.xml |head -n1|sed -e 's,[^>]*>\([^<]*\).*,\1,g')
-s=$(grep '<version>' $rt/../pom.xml |head -n1|sed -e 's,[^>]*>\([^<]*\).*,\1,g')
+p=$(grep '<artifactId>' ${rt}/../pom.xml |head -n1|sed -e 's,[^>]*>\([^<]*\).*,\1,g')
+s=$(grep '<version>' ${rt}/../pom.xml |head -n1|sed -e 's,[^>]*>\([^<]*\).*,\1,g')
 mvn -Plinode package war:exploded  &&
 {
 set -x
 CP=$(mvn dependency:build-classpath |grep  -v '\[INFO\]'|grep -v ' ')
-RD=$rt/..
-TD=$RD/target/${p}-${s}/
-pushd $TD
+RD=${rt}/..
+TD=${RD}/target/${p}-${s}/
+pushd ${TD}
 TD=$(pwd -P)
-java -Xmx425m -cp $CP:$TD/WEB-INF/classes ro.server.KernelImpl
+PATHSEP=":"
+[[ ${OS} = Windows_NT ]] && PATHSEP=';'
+FP="WEB-INF/classes${PATHSEP}${CP}"
+java -Xmx126m -cp "${FP}" ro.server.KernelImpl
 popd
+
 }
