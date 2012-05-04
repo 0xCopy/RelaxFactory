fab0857fd3345bd09f71713b3d4686ad610775bf
Jim Northrup <northrup.james@gmail.com>
2012-05-04 13:26:18 -0700

    brought over the /i/ passthrough for images,text

--- src/main/java/ro/server/RfPostWrapper.java
+++ src/main/java/ro/server/RfPostWrapper.java
@@ -11,8 +11,12 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.text.MessageFormat;
+import java.util.EnumMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.google.web.bindery.requestfactory.server.ServiceLayer;
 import com.google.web.bindery.requestfactory.server.SimpleRequestProcessor;
@@ -23,7 +27,15 @@ import one.xio.HttpMethod;
 import one.xio.MimeType;
 
 import static java.lang.Math.min;
+import static java.nio.channels.SelectionKey.OP_CONNECT;
+import static java.nio.channels.SelectionKey.OP_READ;
+import static java.nio.channels.SelectionKey.OP_WRITE;
+import static one.xio.HttpMethod.GET;
 import static one.xio.HttpMethod.UTF8;
+import static ro.server.KernelImpl.EXECUTOR_SERVICE;
+import static ro.server.KernelImpl.getReceiveBufferSize;
+import static ro.server.KernelImpl.moveCaretToDoubleEol;
+import static ro.server.KernelImpl.recycleChannel;
 
 /**
  * a POST interception wrapper for http protocol cracking
@@ -32,7 +44,123 @@ import static one.xio.HttpMethod.UTF8;
  * Time: 12:37 PM
  */
 class RfPostWrapper extends Impl {
+  public static void main(String... a) {
+    final Pattern compile = Pattern.compile("^/i(/.*)$");
+
+    final String input = "/i/witness/1807e8ec5bcec14842a1a434d300d35c/photo.JPG";
+    final Matcher matcher = compile.matcher(input);
+
+    final boolean matches = matcher.matches();
+    final String group = matcher.group(1);
+    System.err.println("found link: " + group);
+  }
+
   public static final SimpleRequestProcessor SIMPLE_REQUEST_PROCESSOR = new SimpleRequestProcessor(ServiceLayer.create());
+
+
+  public static final EnumMap<HttpMethod, LinkedHashMap<Pattern, AsioVisitor>> NAMESPACE = new EnumMap<HttpMethod, LinkedHashMap<Pattern, AsioVisitor>>(HttpMethod.class) {
+    {
+      final Pattern passthroughExpr = Pattern.compile("^/i(/.*)$");
+      put(GET, new LinkedHashMap<Pattern, AsioVisitor>() {
+        {
+          put(passthroughExpr, new Impl() {
+            @Override
+            public void onWrite(final SelectionKey browserKey) throws Exception {
+
+              browserKey.interestOps(OP_READ);
+              String path;
+              ByteBuffer headers;
+              final ByteBuffer dst;
+              //receives impl,path,headers,first block
+              final Object attachment = browserKey.attachment();
+              if (!(attachment instanceof Object[])) {
+                throw new UnsupportedOperationException("this GET proxy requires attach(this,path,headers,block0) to function correctly");
+              }
+              Object[] objects = (Object[]) attachment;
+              path = (String) objects[1];
+              headers = (ByteBuffer) objects[2];
+              dst = (ByteBuffer) objects[3];
+              final Matcher matcher = passthroughExpr.matcher(path);
+              if (matcher.matches()) {
+                String link = matcher.group(1);
+
+                final String req = "GET " + link + " HTTP/1.1\r\n" +
+                    "Accept: image/*, text/*\r\n" +
+//                    "Connection: close\r\n" +
+                    "\r\n";
+
+                final SocketChannel couchConnection = KernelImpl.createCouchConnection();
+                HttpMethod.enqueue(couchConnection, OP_CONNECT | OP_WRITE,
+                    new Impl() {
+                      @Override
+                      public void onRead(final SelectionKey couchKey) throws Exception {
+                        final SocketChannel channel = (SocketChannel) couchKey.channel();
+                        channel.read((ByteBuffer) dst.clear());
+                        moveCaretToDoubleEol((ByteBuffer) dst.flip());
+                        ByteBuffer headers = ((ByteBuffer) dst.duplicate().flip()).slice();
+
+                        final Map<String, int[]> map = HttpHeaders.getHeaders((ByteBuffer) headers.rewind());
+                        final int[] ints = map.get("Content-Length");
+                        final int total = Integer.parseInt(UTF8.decode((ByteBuffer) headers.duplicate().clear().position(ints[0]).limit(ints[1])).toString().trim());
+                        final SocketChannel browserChannel = (SocketChannel) browserKey.channel();
+                        browserChannel.write((ByteBuffer) headers.rewind());
+
+                        couchKey.interestOps(OP_READ).attach(new Impl() {
+                          final ByteBuffer sharedBuf = ByteBuffer.allocateDirect(min(total, getReceiveBufferSize()));
+                          private Impl browserSlave = new Impl() {
+                            @Override
+                            public void onWrite(SelectionKey key) throws Exception {
+                              final int write = browserChannel.write(dst);
+//                              System.err.println("sending " + UTF8.decode((ByteBuffer) dst.duplicate().rewind().limit(40)).toString().trim());
+                              if (remaining > 0) {
+                                couchKey.interestOps(OP_READ);
+                                browserKey.interestOps(0).selector().wakeup();
+                              } else if (!dst.hasRemaining()) {
+                                browserKey.interestOps(OP_READ).attach(null);
+                                browserKey.interestOps(0).selector().wakeup();
+                              }
+                            }
+                          };
+                          public int remaining = total; {
+                            browserKey.attach(browserSlave);
+                          }
+
+                          @Override
+                          public void onRead(final SelectionKey couchKey) throws Exception {
+                            if (remaining > 0) {
+                              dst.compact();
+                              final int read = couchConnection.read(dst);
+                              remaining -= read;
+                              dst.flip();
+                              couchKey.interestOps(0);
+                              browserKey.interestOps(OP_WRITE).selector().wakeup();
+                            } else {
+                              recycleChannel(couchConnection);
+                            }
+                          }
+                        });
+
+
+                      }
+
+                      @Override
+                      public void onWrite(SelectionKey couchKey) throws Exception {
+                        couchConnection.write(UTF8.encode(req));
+                        couchKey.interestOps(OP_READ);
+                      }
+                    });
+
+
+              }
+            }
+
+          });
+        }
+      });
+
+
+    }
+  };
 
   @Override
   public void onRead(final SelectionKey key) throws IOException {
@@ -90,7 +218,7 @@ class RfPostWrapper extends Impl {
               public void onRead(SelectionKey selectionKey) throws IOException {
                 ((SocketChannel) selectionKey.channel()).read(finalDst);
                 if (!finalDst.hasRemaining()) {
-                  KernelImpl.EXECUTOR_SERVICE.submit(new RfProcessTask(finalHeaders, finalDst, key));
+                  EXECUTOR_SERVICE.submit(new RfProcessTask(finalHeaders, finalDst, key));
                 }
               }
             });
@@ -103,41 +231,72 @@ class RfPostWrapper extends Impl {
           while (!Character.isWhitespace(headers.get())) ;
           int position = headers.position();
           while (!Character.isWhitespace(headers.get())) ;
-          String fname = URLDecoder.decode(UTF8.decode((ByteBuffer) headers.flip().position(position)).toString().trim());
 
-          fname = MessageFormat.format("./{0}", fname.split("[\\#\\?]")).replace("//", "/").replace("../", "./");
+          String path = URLDecoder.decode(UTF8.decode((ByteBuffer) headers.flip().position(position)).toString().trim());
+          for (Map.Entry<Pattern, AsioVisitor> visitorEntry : NAMESPACE.get(method).entrySet()) {
+            final Matcher matcher = visitorEntry.getKey().matcher(path);
+            final boolean b = matcher.find();
+            if (b) {
+
+              key.interestOps(OP_CONNECT | OP_WRITE).attach(
+                  /**sends impl,path,headers,first block
+                   */
+                  new Object[]{visitorEntry.getValue(), path, headers, dst.rewind()});
+              key.selector().wakeup();
+              return;
+            }
+          }
+          String fname = MessageFormat.format("./{0}", path.split("[\\#\\?]")).replace("//", "/").replace("../", "./");
+
           final File filex = new File(fname);
           final File[] file = {filex};
-          if (file[0].isFile()) {
-            final String finalFname = fname;
-            key.interestOps(SelectionKey.OP_WRITE);
-            key.attach(new Impl() {
 
-              @Override
-              public void onWrite(SelectionKey key) throws Exception {
+          final String finalFname = fname;
+          key.interestOps(OP_WRITE);
+          key.attach(new Impl() {
 
+            @Override
+            public void onWrite(SelectionKey key) throws Exception {
+              final SocketChannel socketChannel = (SocketChannel) key.channel();
+              final int sendBufferSize = KernelImpl.getSendBufferSize();
+              String ceString = "";
 
-                final SocketChannel socketChannel = (SocketChannel) key.channel();
-                final int sendBufferSize = KernelImpl.getSendBufferSize();
-                String ceString = "";
-
-                Map<String, int[]> hmap = HttpHeaders.getHeaders((ByteBuffer) headers.clear());
-                int[] ints = hmap.get("Accept-Encoding");
-                if (null != ints) {
-                  String accepts = UTF8.decode((ByteBuffer) headers.clear().limit(ints[1]).position(ints[0])).toString().trim();
-                  for (CompressionTypes compTypes : CompressionTypes.values()) {
-                    if (accepts.contains(compTypes.name())) {
-                      File file1 = new File(finalFname + "." + compTypes.suffix);
-                      if (file1.isFile()) {
-                        file[0] = file1;
-                        System.err.println("sending compressed archive: " + file1.getAbsolutePath());
-                        ceString = MessageFormat.format("Content-Encoding: {0}\r\n", compTypes.name());
-                        break;
-                      }
+              Map<String, int[]> hmap = HttpHeaders.getHeaders((ByteBuffer) headers.clear());
+              int[] ints = hmap.get("Accept-Encoding");
+              if (null != ints) {
+                String accepts = UTF8.decode((ByteBuffer) headers.clear().limit(ints[1]).position(ints[0])).toString().trim();
+                for (CompressionTypes compType : CompressionTypes.values()) {
+                  if (accepts.contains(compType.name())) {
+                    File file1 = new File(finalFname + "." + compType.suffix);
+                    if (file1.isFile()) {
+                      file[0] = file1;
+                      System.err.println("sending compressed archive: " + file1.getAbsolutePath());
+                      ceString = MessageFormat.format("Content-Encoding: {0}\r\n", compType.name());
+                      break;
                     }
                   }
                 }
+              }
+              boolean send200 = false;
+              try {
+                send200 = file[0].canRead();
+              } finally {
 
+              }
+
+              if (!send200) {
+                key.interestOps(OP_WRITE).attach(new Impl() {
+                  @Override
+                  public void onWrite(SelectionKey key) throws Exception {
+
+                    String response = "HTTP/1.1 404 Not Found\n" +
+                        "Content-Length: 0\n\n";
+                    int write = socketChannel.write(UTF8.encode(response));
+                    key.interestOps(OP_READ).attach(RfPostWrapper.this);
+                    key.selector().wakeup();
+                  }
+                });
+              } else {
                 final RandomAccessFile randomAccessFile = new RandomAccessFile(file[0], "r");
                 final long total = randomAccessFile.length();
                 final FileChannel fileChannel = randomAccessFile.getChannel();
@@ -153,13 +312,9 @@ class RfPostWrapper extends Impl {
                   length = randomAccessFile.length();
                 }
                 String response = MessageFormat.format("HTTP/1.1 200 OK\r\nContent-Type: {0}\r\nContent-Length: {1,number,#}\r\n{2}\r\n", (null == mimeType ? MimeType.bin : mimeType).contentType, length, ceString);
-//                System.err.println("GET response: " + response + '+');
-
-
                 int write = socketChannel.write(UTF8.encode(response));
-
                 final long[] progress = {fileChannel.transferTo(0, sendBufferSize, socketChannel)};
-                key.interestOps(SelectionKey.OP_WRITE);
+                key.interestOps(OP_WRITE | OP_CONNECT);
                 key.attach(new Impl() {
                   @Override
                   public void onWrite(SelectionKey key) throws Exception {
@@ -169,14 +324,14 @@ class RfPostWrapper extends Impl {
                     if (remaining == 0) {
                       fileChannel.close();
                       randomAccessFile.close();
-                      key.interestOps(SelectionKey.OP_READ);
+                      key.interestOps(OP_READ);
                       key.attach(new Object[0]);
                     }
                   }
                 });
               }
-            });
-          }
+            }
+          });
           break;
         }
         default:
@@ -197,7 +352,7 @@ class RfPostWrapper extends Impl {
     ServerSocketChannel channel = (ServerSocketChannel) key.channel();
     SocketChannel accept = channel.accept();
     accept.configureBlocking(false);
-    HttpMethod.enqueue(accept, SelectionKey.OP_READ, this);
+    HttpMethod.enqueue(accept, OP_READ, this);
 
   }
 
@@ -233,7 +388,7 @@ class RfPostWrapper extends Impl {
           ((SocketChannel) key.channel()).write(UTF8.encode(s1 + process));
           System.err.println("debug: " + s1 + process);
           key.attach(null);
-          key.interestOps(SelectionKey.OP_READ);
+          key.interestOps(OP_READ);
         }
       });
       key.interestOps(SelectionKey.OP_WRITE);
