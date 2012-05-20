18e40d7eaf275573cf66f094b38d3244f9bbcbcc
Jim Northrup <northrup.james@gmail.com>
2012-05-19 22:11:23 -0700

    this version is writing logins

---
+++ rxf-server/src/main/java/rxf/server/Rfc822HeaderPrefix.java
@@ -0,0 +1,83 @@
+package rxf.server;
+
+import java.nio.ByteBuffer;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import one.xio.HttpHeaders;
+
+import static one.xio.HttpMethod.UTF8;
+import static rxf.server.BlobAntiPatternObject.moveCaretToDoubleEol;
+
+/**
+ * long overdue utility class to parse a request header or response header according to declared need of header/cookie
+ * <p/>
+ * preload headers and cookies, send response and request initial onRead for .apply()
+ * <p/>
+ * User: jim
+ * Date: 5/19/12
+ * Time: 10:00 PM
+ */
+public class Rfc822HeaderPrefix {
+  private String[] headers;
+  private String[] cookies;
+  private ByteBuffer cursor;
+  private ByteBuffer headerBuf;
+  private Map headerStrings;
+  private Map cookieStrings;
+  private String rescode;
+
+  public Rfc822HeaderPrefix(String[] headers, String... cookies) {
+
+    this.headers = headers;
+    this.cookies = cookies;
+  }
+
+  public ByteBuffer getCursor() {
+    return cursor;
+  }
+
+  public ByteBuffer getHeaderBuf() {
+    return headerBuf;
+  }
+
+  public Map getHeaderStrings() {
+    return headerStrings;
+  }
+
+  public Map getCookieStrings() {
+    return cookieStrings;
+  }
+
+  public String getRescode() {
+    return rescode;
+  }
+
+  public Rfc822HeaderPrefix apply(ByteBuffer cursor) {
+
+    while (cursor.get() != ' ') ;
+    final ByteBuffer slice = cursor.slice();
+    while (slice.get() != ' ') ;
+    headerBuf = null;
+    final boolean wantsCookies = cookies.length > 1;
+    final boolean wantsHeaders = wantsCookies || headers.length > 1;
+    headerBuf = (ByteBuffer) moveCaretToDoubleEol(cursor).duplicate().flip();
+    headerStrings = null;
+    cookieStrings = null;
+    if (wantsHeaders) {
+      Map headerMap = HttpHeaders.getHeaders((ByteBuffer) headerBuf.rewind());
+      headerStrings = new LinkedHashMap<String, List>();
+      for (Object o : headerMap.keySet()) {
+        headerStrings.put(o, headerMap.get(o));
+      }
+      if (wantsCookies) {
+        cookieStrings = new LinkedHashMap();
+        //todo something to parse cookies
+        cookieStrings.put("", "");
+      }
+    }
+    rescode = UTF8.decode((ByteBuffer) slice.flip()).toString().trim();
+    return this;
+  }
+}
