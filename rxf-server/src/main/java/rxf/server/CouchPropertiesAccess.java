489ef8b33deb538fe46a44f48a575ec0ac8dfeaa
Jim Northrup <northrup.james@gmail.com>
2012-06-01 21:27:48 -0700

    begining login usecase repair

--- rxf-server/src/main/java/rxf/server/CouchPropertiesAccess.java
+++ rxf-server/src/main/java/rxf/server/CouchPropertiesAccess.java
@@ -1,7 +1,5 @@
 package rxf.server;
 
-import static rxf.server.BlobAntiPatternObject.setGenericDocumentProperty;
-
 /**
  * User: jim
  * Date: 5/10/12
@@ -15,32 +13,4 @@ public class CouchPropertiesAccess<T> {
     this.locator = locator;
   }
 
-  public String getSessionProperty(String eid, String key) {
-    try {
-      String path = locator.getPathPrefix() + '/' + eid;
-      return BlobAntiPatternObject.getGenericDocumentProperty(path, key);
-    } catch (Exception e) {
-      e.printStackTrace();  //todo: verify for a purpose
-    }
-    return null;
-  }
-
-  /**
-   * @return new version string
-   */
-  public String setSessionProperty(String eid, String key, String value) {
-
-    String path = locator.getPathPrefix() + '/' + eid;
-    CouchTx tx = null;
-    try {
-      tx = setGenericDocumentProperty(path, key, value);
-      System.err.println("tx: " + tx.toString());
-      return tx.rev();
-    } catch (Exception e) {
-      e.printStackTrace();  //todo: verify for a purpose
-    }
-
-
-    return eid;
-  }
 }
--- rxf-server/src/main/java/rxf/server/VisitorPropertiesAccess.java
+++ rxf-server/src/main/java/rxf/server/VisitorPropertiesAccess.java
@@ -1,5 +1,12 @@
 package rxf.server;
 
+import java.util.Map;
+
+import rxf.server.CouchDriver.getDocBuilder;
+import rxf.server.CouchDriver.updateDocBuilder;
+
+import static rxf.server.BlobAntiPatternObject.GSON;
+
 /**
  * User: jim
  * Date: 4/20/12
@@ -7,23 +14,20 @@ package rxf.server;
  */
 public class VisitorPropertiesAccess {
 
-  private static CouchPropertiesAccess<Visitor> visitorCouchPropertiesAccess = new CouchPropertiesAccess<Visitor>(BlobAntiPatternObject.VISITOR_LOCATOR);
-
-  static public String getSessionProperty(String key) {
-    try {
-      return visitorCouchPropertiesAccess.getSessionProperty(BlobAntiPatternObject.getSessionCookieId(), key);
-    } catch (Exception e) {
-      e.printStackTrace();  //todo: verify for a purpose
-    }
-    return key;
+  static public String getSessionProperty(String key) throws Exception {
+    String doc = new getDocBuilder().db(Visitor.createLocator().getPathPrefix()).docId(BlobAntiPatternObject.getSessionCookieId()).to().fire().pojo();
+    return String.valueOf(GSON.fromJson(doc, Map.class).get(key));
   }
 
-  static public String setSessionProperty(String key, String value) {
-    try {
-      return new CouchPropertiesAccess<Visitor>(BlobAntiPatternObject.VISITOR_LOCATOR).setSessionProperty(BlobAntiPatternObject.getSessionCookieId(), key, value);
-    } catch (Exception e) {
-      e.printStackTrace();  //todo: verify for a purpose
-    }
-    return key;
+  static public String setSessionProperty(String key, String value) throws Exception {
+
+    String eid = BlobAntiPatternObject.getSessionCookieId();
+    String doc = new getDocBuilder().db(Visitor.createLocator().getPathPrefix()).docId(eid).to().fire().pojo();
+    Map map = GSON.fromJson(doc, Map.class);
+    map.put(key, value);
+    String s = GSON.toJson(map);
+    CouchTx tx = new updateDocBuilder().db(doc).docId(eid).rev(ActionBuilder.get().state().dequotedHeader(CouchMetaDriver.ETAG)).to().fire().tx();
+    return GSON.toJson(tx);
+
   }
 }
