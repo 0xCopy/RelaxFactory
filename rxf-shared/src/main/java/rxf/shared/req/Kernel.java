8f982c48dd636ed5afb360e9ce05cec17fc4084d
Jim Northrup <northrup.james@gmail.com>
2012-06-11 16:17:41 -0700

    removed Visitor pojo and rm'd the bitrot example tree

--- rxf-shared/src/main/java/rxf/shared/req/Kernel.java
+++ rxf-shared/src/main/java/rxf/shared/req/Kernel.java
@@ -2,13 +2,11 @@
 
 package rxf.shared.req;
 
-import com.google.web.bindery.requestfactory.shared.Request;
 import com.google.web.bindery.requestfactory.shared.RequestContext;
 import com.google.web.bindery.requestfactory.shared.ServiceName;
-import rxf.shared.prox.VisitorProxy;
 
 
 @ServiceName("rxf.server.BlobAntiPatternObject")
 public interface Kernel extends RequestContext {
-  Request<VisitorProxy> getCurrentSession();
+
 }
