6b9a9778f64e43894586711924f095cd89ad0ffa
Colin Alworth <niloc132@gmail.com>
2012-07-02 23:54:48 -0500

    Working system to build CouchServices upon request, provided they are all named when guice is configured.

--- rxf-guice/src/main/java/rxf/server/guice/CouchServiceProvider.java
+++ rxf-guice/src/main/java/rxf/server/guice/CouchServiceProvider.java
@@ -6,28 +6,18 @@ import rxf.server.CouchService;
 import rxf.server.CouchServiceFactory;
 
 import com.google.inject.Inject;
-import com.google.inject.TypeLiteral;
+import com.google.inject.Provider;
 import com.google.inject.name.Named;
-import com.google.inject.spi.BindingTargetVisitor;
-import com.google.inject.spi.ProviderInstanceBinding;
-import com.google.inject.spi.ProviderWithExtensionVisitor;
 
-public class CouchServiceProvider<T extends CouchService<?>> implements ProviderWithExtensionVisitor<T> {
-  private String namespace;
-  private Class<T> type;
+public class CouchServiceProvider<T extends CouchService<?>> implements Provider<T> {
   @Inject
-  public CouchServiceProvider(@Named("namespace") String namespace) {
-    this.namespace = namespace;
+  @Named(CouchModuleBuilder.NAMESPACE)
+  private String namespace;
+  private final Class<T> type;
+  public CouchServiceProvider(Class<T> serviceType) {
+    this.type = serviceType;
   }
-  
-  public <B, V> V acceptExtensionVisitor(BindingTargetVisitor<B, V> visitor, ProviderInstanceBinding<? extends B> binding) {
-    // "In practice, the 'B' type will always be a supertype of 'T'."
-    TypeLiteral<?> k = binding.getKey().getTypeLiteral();
-    this.type = (Class<T>) k.getRawType().asSubclass(CouchService.class);
-    
-    return visitor.visit(binding);
-  }
-  
+
   public T get() {
     try {
       return CouchServiceFactory.get(type, namespace);
