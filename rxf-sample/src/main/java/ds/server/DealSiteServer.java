5dd140d18d20d7b029e11e8c43db44d3abcb50de
Jim Northrup <jim@fusion.(none)>
2012-07-17 23:24:02 -0700

    fixed juxtaposed ContentLength in GwtRfVisitor where rf return payload is larger than the original payload (missed in unit tests)

--- rxf-sample/src/main/java/ds/server/DealSiteServer.java
+++ rxf-sample/src/main/java/ds/server/DealSiteServer.java
@@ -1,23 +1,26 @@
 package ds.server;
 
+import ds.model.*;
+import one.xio.AsioVisitor.Impl;
+import one.xio.HttpMethod;
+import rxf.server.BlobAntiPatternObject;
+import rxf.server.CouchLocator;
+import rxf.server.CouchServiceFactory;
+import rxf.server.CouchTx;
+
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
 import java.util.*;
-import java.util.concurrent.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import ds.model.*;
-import one.xio.AsioVisitor.Impl;
-import one.xio.HttpMethod;
-import rxf.server.*;
-
-import static ds.server.SecurityImpl.DEAL_COUCH_LOCATOR;
-import static ds.server.SecurityImpl.NPO_COUCH_LOCATOR;
-import static ds.server.SecurityImpl.VENDOR_COUCH_LOCATOR;
+import static ds.server.SecurityImpl.*;
 import static java.lang.Math.abs;
-import static one.xio.HttpMethod.GET;
 import static one.xio.HttpMethod.UTF8;
 import static rxf.server.BlobAntiPatternObject.deepToString;
 import static rxf.server.CouchNamespace.NAMESPACE;
@@ -29,171 +32,161 @@ import static rxf.server.CouchNamespace.NAMESPACE;
  */
 public class DealSiteServer {
 
-  public static final Pattern FRAGMENT = Pattern.compile(".*_escaped_fragment_=([^&]+)");
-
-  /**
-   * the MotdServlet
-   *
-   * registers a protocol level socket handler based on AsioVisitor interface.
-   *
-   * this one shows a non-http response to dump a file quick and dirty with no sanity checks
-   *
-   */
-  static {
+    public static final Pattern FRAGMENT = Pattern.compile(".*_escaped_fragment_=([^&]+)");
 
 
-
-  }
-
-  public static void registerurlExprAsFirst(HttpMethod method, final Pattern passthroughExpr, final Class<? extends Impl> value) {
-    LinkedHashMap<Pattern, Class<? extends Impl>> linkedHashMap = new LinkedHashMap<Pattern, Class<? extends Impl>>();
-    linkedHashMap.put(passthroughExpr, value);
-    Map<Pattern, Class<? extends Impl>> patternImplMap = NAMESPACE.get(method);
-    if (null != patternImplMap) linkedHashMap.putAll(patternImplMap);
-    NAMESPACE.put(method, linkedHashMap);
-  }
-
-  public static void main(String... args) throws Exception {
-    if (System.getenv().containsKey("DEBUG_DEAL_EDITORS")) {
-      BlobAntiPatternObject.EXECUTOR_SERVICE.schedule(new Runnable() {
-        public void run() {
-          CouchLocator<Npo> npoLocator = NPO_COUCH_LOCATOR;
-          CouchLocator<Vendor> vlocator = VENDOR_COUCH_LOCATOR;
-          CouchLocator<Deal> dlocator = DEAL_COUCH_LOCATOR;
-          Deal deal = dlocator.create(Deal.class);
-          deal.setProduct(UUID.randomUUID().toString());
-          deal.productDescription = word() + ' ' + word() + ' ' + word() + ' ' + word() + ' ' + word() + ' ' + word() + ' ' + word() + ".  " + word() + ' ' + word() + ' ' + word() + ' ' + word() + ' ' + word() + ' ' + word() + ' ' + word() + '!'; //UUID.randomUUID().toString()+' ' +UUID.randomUUID().toString()+' ' +UUID.randomUUID().toString()+' ' +UUID.randomUUID().toString()+".  " +UUID.randomUUID().toString()+' ' +UUID.randomUUID().toString()+' ' +UUID.randomUUID().toString()+'!' ;
-          deal.setLimit(new Random().nextInt(150) + 50);
-          deal.setMinimum(new Random().nextInt(48) + 2);
-
-          Vendor v = new Vendor();
-          v.setName(word());
-          v.setContactInfo(new Contact());
-          v.getContactInfo().setAddr1("1234 Main St");
-          v.getContactInfo().setCity("Somewhere");
-          v.getContactInfo().setState("Any State");
-          v.getContactInfo().setEmail("person@company.com");
-          v.getContactInfo().setPhone("321-654-0987");
-          v.setCreation(new Date());
-          v.setDescription(word() + " " + word() + " " + word());
-          v.setPocName("???");
-
-          Npo npo = new Npo();
-          npo.setContactInfo(new Contact());
-          npo.getContactInfo().setAddr1("987 Main St");
-          npo.getContactInfo().setCity("Elsewhere");
-          npo.getContactInfo().setState("Other State");
-          npo.getContactInfo().setEmail("person@cause.org");
-          npo.getContactInfo().setPhone("890-567-1234");
-          npo.setCreation(new Date());
-          npo.setDescription(word() + " " + word());
-          npo.setName(word());
-          npo.setPocName("???");
-
-          try {
-            CouchTx persist = npoLocator.persist(npo);
-            System.err.println(persist.toString());
-            String id = persist.id();
-            deal.setNpoId(id);
-
-            persist = vlocator.persist(v);
-            System.err.println(persist.toString());
-            deal.setVendorId(id);
-
-            persist = dlocator.persist(deal);
-            System.err.println(persist.toString());
-          } catch (Exception e) {
-            e.printStackTrace();
-          }
+    public static void registerurlExprAsFirst(HttpMethod method, Pattern passthroughExpr, Class<? extends Impl> value) {
+        Map<Pattern, Class<? extends Impl>> linkedHashMap = new LinkedHashMap<Pattern, Class<? extends Impl>>();
+        linkedHashMap.put(passthroughExpr, value);
+        Map<Pattern, Class<? extends Impl>> patternImplMap = NAMESPACE.get(method);
+        if (null != patternImplMap) {
+            linkedHashMap.putAll(patternImplMap);
         }
-      }, 5, TimeUnit.SECONDS);
-      BlobAntiPatternObject.EXECUTOR_SERVICE.schedule(new Runnable() {
-        public void run() {
-          DealService service = null;
-          try {
-            service = CouchServiceFactory.get(DealService.class);
-          } catch (InterruptedException e) {
-            e.printStackTrace();
-          } catch (ExecutionException e) {
-            e.printStackTrace();
-          }
-          List<Deal> lawnmower = service.findByProduct("lawnmower");
-          System.err.println(deepToString(lawnmower));
-        }
-      }, 5, TimeUnit.SECONDS);
+        NAMESPACE.put(method, linkedHashMap);
     }
 
-    BlobAntiPatternObject.EXECUTOR_SERVICE.schedule(new Runnable() {
-      @Override
-      public void run() {
-        registerurlExprAsFirst(HttpMethod.GET, FRAGMENT, FragMent.class /*(fragment)*/);
+    public static void main(String... args) throws Exception {
+        if (System.getenv().containsKey("DEBUG_DEAL_EDITORS")) {
+            BlobAntiPatternObject.EXECUTOR_SERVICE.schedule(new Runnable() {
+                public void run() {
+                    CouchLocator<Npo> npoLocator = NPO_COUCH_LOCATOR;
+                    CouchLocator<Vendor> vlocator = VENDOR_COUCH_LOCATOR;
+                    CouchLocator<Deal> dlocator = DEAL_COUCH_LOCATOR;
+                    Deal deal = dlocator.create(Deal.class);
+                    deal.setProduct(UUID.randomUUID().toString());
+                    deal.productDescription = word() + ' ' + word() + ' ' + word() + ' ' + word() + ' ' + word() + ' ' + word() + ' ' + word() + ".  " + word() + ' ' + word() + ' ' + word() + ' ' + word() + ' ' + word() + ' ' + word() + ' ' + word() + '!'; //UUID.randomUUID().toString()+' ' +UUID.randomUUID().toString()+' ' +UUID.randomUUID().toString()+' ' +UUID.randomUUID().toString()+".  " +UUID.randomUUID().toString()+' ' +UUID.randomUUID().toString()+' ' +UUID.randomUUID().toString()+'!' ;
+                    deal.setLimit(new Random().nextInt(150) + 50);
+                    deal.setMinimum(new Random().nextInt(48) + 2);
 
-          Pattern authpat = Pattern.compile("/rxf.server.Auth/.*");
-          registerurlExprAsFirst(HttpMethod.GET, authpat, OAuthHandler.class /*(fragment)*/);
-          registerurlExprAsFirst(HttpMethod.POST, authpat, OAuthHandler.class /*(fragment)*/);
+                    Vendor v = new Vendor();
+                    v.setName(word());
+                    v.setContactInfo(new Contact());
+                    v.getContactInfo().setAddr1("1234 Main St");
+                    v.getContactInfo().setCity("Somewhere");
+                    v.getContactInfo().setState("Any State");
+                    v.getContactInfo().setEmail("person@company.com");
+                    v.getContactInfo().setPhone("321-654-0987");
+                    v.setCreation(new Date());
+                    v.setDescription(word() + " " + word() + " " + word());
+                    v.setPocName("???");
 
-      }
-    }, 250,TimeUnit.MILLISECONDS);
-    BlobAntiPatternObject.startServer(args);
+                    Npo npo = new Npo();
+                    npo.setContactInfo(new Contact());
+                    npo.getContactInfo().setAddr1("987 Main St");
+                    npo.getContactInfo().setCity("Elsewhere");
+                    npo.getContactInfo().setState("Other State");
+                    npo.getContactInfo().setEmail("person@cause.org");
+                    npo.getContactInfo().setPhone("890-567-1234");
+                    npo.setCreation(new Date());
+                    npo.setDescription(word() + " " + word());
+                    npo.setName(word());
+                    npo.setPocName("???");
 
-  }
+                    try {
+                        CouchTx persist = npoLocator.persist(npo);
+                        System.err.println(persist.toString());
+                        String id = persist.id();
+                        deal.setNpoId(id);
 
-  private static String word() {
-    return Long.toString(abs(new Random().nextLong()), 36);
-  }
+                        persist = vlocator.persist(v);
+                        System.err.println(persist.toString());
+                        deal.setVendorId(id);
 
-  static class FragMent extends Impl {
-
-    @Override
-    public void onWrite(final SelectionKey key) throws Exception {
-      Callable<String> callable = new Callable<String>() {
-
-
-        public String call() throws Exception {
-          DealSiteServerActivities activities = new DealSiteServerActivities();
-          Object[] args = (Object[]) key.attachment();
-          String path = (String) args[1];
-          Matcher m = FRAGMENT.matcher(path);
-          m.find();
-          String f = m.group(1);
-
-          try {
-            String results = activities.generateContentForToken(f);
-
-
-            final ByteBuffer responseBuffer = UTF8.encode(results);
-            int length = responseBuffer.limit();// counting 16 bit chars could get us in trouble
-            String s1 = "HTTP/1.1 200 OK\r\n" +
-                "Content-Type: text/html ; charset=utf-8\r\n" +
-                "Content-Length: " + length + "\r\n\r\n";
-
-            final SocketChannel channel = (SocketChannel) key.channel();
-
-            ByteBuffer respHdrs = UTF8.encode(s1);
-            int written = channel.write(respHdrs);
-            // there is a chance that somehow this line of text will not fully
-            // write.  would be interesting to see when.
-            assert respHdrs.limit() == written;
-            key.selector().wakeup();//removes 1000ms from transitions from READ<->WRITE
-            key.interestOps(SelectionKey.OP_WRITE | SelectionKey.OP_CONNECT).attach(new Impl() {
-              @Override
-              public void onWrite(SelectionKey key) throws Exception {
-                int write = channel.write(responseBuffer);
-                if (!responseBuffer.hasRemaining())
-                  key.selector().wakeup();//removes 1000ms from transitions from READ<->WRITE
-                key.interestOps(SelectionKey.OP_READ).attach(null);//http pipeline takes over the selector
-              }
-            });
-          } catch (Throwable e) {
-            e.printStackTrace();
-            key.channel().close();
-          }
-
-          return null;
+                        persist = dlocator.persist(deal);
+                        System.err.println(persist.toString());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, 5, TimeUnit.SECONDS);
+            BlobAntiPatternObject.EXECUTOR_SERVICE.schedule(new Runnable() {
+                public void run() {
+                    DealService service = null;
+                    try {
+                        service = CouchServiceFactory.get(DealService.class);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    }
+                    List<Deal> lawnmower = service.findByProduct("test");
+                    System.err.println(deepToString(lawnmower));
+                }
+            }, 5, TimeUnit.SECONDS);
         }
-      };
-      Future<String> submit = BlobAntiPatternObject.EXECUTOR_SERVICE.submit(callable);
-      key.interestOps(0);
+
+        BlobAntiPatternObject.EXECUTOR_SERVICE.schedule(new Runnable() {
+            @Override
+            public void run() {
+                registerurlExprAsFirst(HttpMethod.GET, FRAGMENT, FragMent.class /*(fragment)*/);
+
+                Pattern authpat = Pattern.compile("/rxf.server.Auth/.*");
+                registerurlExprAsFirst(HttpMethod.GET, authpat, OAuthHandler.class /*(fragment)*/);
+                registerurlExprAsFirst(HttpMethod.POST, authpat, OAuthHandler.class /*(fragment)*/);
+
+            }
+        }, 250, TimeUnit.MILLISECONDS);
+        BlobAntiPatternObject.startServer(args);
+
     }
-  }
+
+    private static String word() {
+        return Long.toString(abs(new Random().nextLong()), 36);
+    }
+
+    static class FragMent extends Impl {
+
+        @Override
+        public void onWrite(final SelectionKey key) throws Exception {
+            Callable<String> callable = new Callable<String>() {
+
+
+                public String call() throws Exception {
+                    DealSiteServerActivities activities = new DealSiteServerActivities();
+                    Object[] args = (Object[]) key.attachment();
+                    String path = (String) args[1];
+                    Matcher m = FRAGMENT.matcher(path);
+                    m.find();
+                    String f = m.group(1);
+
+                    try {
+                        String results = activities.generateContentForToken(f);
+
+
+                        final ByteBuffer responseBuffer = UTF8.encode(results);
+                        int length = responseBuffer.limit();// counting 16 bit chars could get us in trouble
+                        String s1 = "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: text/html ; charset=utf-8\r\n" +
+                                "Content-Length: " + length + "\r\n\r\n";
+
+                        final SocketChannel channel = (SocketChannel) key.channel();
+
+                        ByteBuffer respHdrs = UTF8.encode(s1);
+                        int written = channel.write(respHdrs);
+                        // there is a chance that somehow this line of text will not fully
+                        // write.  would be interesting to see when.
+                        assert respHdrs.limit() == written;
+                        key.selector().wakeup();//removes 1000ms from transitions from READ<->WRITE
+                        key.interestOps(SelectionKey.OP_WRITE | SelectionKey.OP_CONNECT).attach(new Impl() {
+                            @Override
+                            public void onWrite(SelectionKey key) throws Exception {
+                                int write = channel.write(responseBuffer);
+                                if (!responseBuffer.hasRemaining()) {
+                                    key.selector().wakeup();//removes 1000ms from transitions from READ<->WRITE
+                                }
+                                key.interestOps(SelectionKey.OP_READ).attach(null);//http pipeline takes over the selector
+                            }
+                        });
+                    } catch (Throwable e) {
+                        e.printStackTrace();
+                        key.channel().close();
+                    }
+
+                    return null;
+                }
+            };
+            Future<String> submit = BlobAntiPatternObject.EXECUTOR_SERVICE.submit(callable);
+            key.interestOps(0);
+        }
+    }
 }
--- rxf-server/src/main/java/rxf/server/GwtRequestFactoryVisitor.java
+++ rxf-server/src/main/java/rxf/server/GwtRequestFactoryVisitor.java
@@ -1,10 +1,5 @@
 package rxf.server;
 
-import java.nio.Buffer;
-import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.SocketChannel;
-
 import com.google.web.bindery.requestfactory.server.ServiceLayer;
 import com.google.web.bindery.requestfactory.server.SimpleRequestProcessor;
 import one.xio.AsioVisitor.Impl;
@@ -12,10 +7,13 @@ import one.xio.HttpHeaders;
 import one.xio.MimeType;
 import rxf.server.Rfc822HeaderState.HttpRequest;
 
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.SocketChannel;
+
 import static one.xio.HttpMethod.UTF8;
-import static rxf.server.BlobAntiPatternObject.EXECUTOR_SERVICE;
-import static rxf.server.BlobAntiPatternObject.HEADER_TERMINATOR;
-import static rxf.server.BlobAntiPatternObject.getReceiveBufferSize;
+import static rxf.server.BlobAntiPatternObject.*;
 
 /**
  * User: jim
@@ -23,91 +21,96 @@ import static rxf.server.BlobAntiPatternObject.getReceiveBufferSize;
  * Time: 7:42 PM
  */
 public class GwtRequestFactoryVisitor extends Impl implements PreRead {
-  public static SimpleRequestProcessor SIMPLE_REQUEST_PROCESSOR = new SimpleRequestProcessor(ServiceLayer.create());
-  HttpRequest req;
-  ByteBuffer cursor = null;
-  private SocketChannel channel;
-  String payload;
+    public static SimpleRequestProcessor SIMPLE_REQUEST_PROCESSOR = new SimpleRequestProcessor(ServiceLayer.create());
+    HttpRequest req;
+    ByteBuffer cursor = null;
+    private SocketChannel channel;
+    String payload;
 
 
-  @Override
-  public void onRead(SelectionKey key) throws Exception {
-    channel = (SocketChannel) key.channel();
-    if (cursor == null) {
-      if (key.attachment() instanceof Object[]) {
-        Object[] ar = (Object[]) key.attachment();
-        for (Object o : ar) {
-          if (o instanceof ByteBuffer) {
-            cursor = (ByteBuffer) o;
-            continue;
-          }
-          if (o instanceof Rfc822HeaderState) {
-            req = ((Rfc822HeaderState) o).$req();
-          }
+    @Override
+    public void onRead(SelectionKey key) throws Exception {
+        channel = (SocketChannel) key.channel();
+        if (cursor == null) {
+            if (key.attachment() instanceof Object[]) {
+                Object[] ar = (Object[]) key.attachment();
+                for (Object o : ar) {
+                    if (o instanceof ByteBuffer) {
+                        cursor = (ByteBuffer) o;
+                        continue;
+                    }
+                    if (o instanceof Rfc822HeaderState) {
+                        req = ((Rfc822HeaderState) o).$req();
+                    }
+                }
+            }
+            key.attach(this);
         }
-      }
-      key.attach(this);
-    }
-    cursor = null == cursor ? ByteBuffer.allocateDirect(getReceiveBufferSize()) : cursor.hasRemaining() ? cursor : ByteBuffer.allocateDirect(cursor.capacity() << 1).put((ByteBuffer) cursor.rewind());
-    int read = channel.read(cursor);
-    if (read == -1)
-      key.cancel();
-    Buffer flip = cursor.duplicate().flip();
-    req = (HttpRequest) ActionBuilder.get().state().$req().apply((ByteBuffer) flip);
-    if (!BlobAntiPatternObject.suffixMatchChunks(HEADER_TERMINATOR, req.headerBuf())) {
-      return;
-    }
-    cursor = ((ByteBuffer) cursor).slice();
-    int remaining = Integer.parseInt(req.headerString(HttpHeaders.Content$2dLength));
-    final GwtRequestFactoryVisitor prev = this;
-    if (cursor.remaining() != remaining) key.attach(new Impl() {
-      @Override
-      public void onRead(SelectionKey key) throws Exception {
-        int read1 = channel.read(cursor);
-        if (read1 == -1)
-          key.cancel();
-        if (!cursor.hasRemaining()) {
-          key.interestOps(SelectionKey.OP_WRITE).attach(prev);
-          return;
-        }
-      }
-
-    });
-    key.interestOps(SelectionKey.OP_WRITE);
-  }
-
-  @Override
-  public void onWrite(final SelectionKey key) throws Exception {
-    if (payload == null) {
-      key.interestOps(0);
-      EXECUTOR_SERVICE.submit(new Runnable() {
-        @Override
-        public void run() {
-          try {
-
-            payload = SIMPLE_REQUEST_PROCESSOR.process(UTF8.decode((ByteBuffer) cursor.rewind()).toString());
-            ByteBuffer pbuf = (ByteBuffer) UTF8.encode(payload).rewind();
-            ByteBuffer as = req.$res().headerString(HttpHeaders.Content$2dLength, String.valueOf(cursor.limit())).headerString(HttpHeaders.Content$2dType, MimeType.json.contentType).as(ByteBuffer.class);
-            int needed = as.rewind().limit() + pbuf.rewind().limit();
-            cursor = (ByteBuffer) ((ByteBuffer) (cursor.capacity() >= needed ? cursor.clear().limit(needed) : ByteBuffer.allocateDirect(needed))).put(as).put(pbuf).rewind();
-
-            key.interestOps(SelectionKey.OP_WRITE);
-          } catch (Exception e) {
+        cursor = null == cursor ? ByteBuffer.allocateDirect(getReceiveBufferSize()) : cursor.hasRemaining() ? cursor : ByteBuffer.allocateDirect(cursor.capacity() << 1).put((ByteBuffer) cursor.rewind());
+        int read = channel.read(cursor);
+        if (read == -1)
             key.cancel();
-            e.printStackTrace();  //todo: verify for a purpose
-          } finally {
-          }
+        Buffer flip = cursor.duplicate().flip();
+        req = (HttpRequest) ActionBuilder.get().state().$req().apply((ByteBuffer) flip);
+        if (!BlobAntiPatternObject.suffixMatchChunks(HEADER_TERMINATOR, req.headerBuf())) {
+            return;
         }
-      });
-      return;
-    }
-    int write = channel.write(cursor);
-    if (!cursor.hasRemaining()) {
-      /*Socket socket = channel.socket();
-      socket.getOutputStream().flush();
-      socket.close();*/
-      key.interestOps(SelectionKey.OP_READ).attach(null);
+        cursor = ((ByteBuffer) cursor).slice();
+        int remaining = Integer.parseInt(req.headerString(HttpHeaders.Content$2dLength));
+        final GwtRequestFactoryVisitor prev = this;
+        if (cursor.remaining() != remaining) key.attach(new Impl() {
+            @Override
+            public void onRead(SelectionKey key) throws Exception {
+                int read1 = channel.read(cursor);
+                if (read1 == -1)
+                    key.cancel();
+                if (!cursor.hasRemaining()) {
+                    key.interestOps(SelectionKey.OP_WRITE).attach(prev);
+                    return;
+                }
+            }
+
+        });
+        key.interestOps(SelectionKey.OP_WRITE);
     }
 
-  }
+    @Override
+    public void onWrite(final SelectionKey key) throws Exception {
+        if (payload == null) {
+            key.interestOps(0);
+            EXECUTOR_SERVICE.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+
+                        payload = SIMPLE_REQUEST_PROCESSOR.process(UTF8.decode((ByteBuffer) cursor.rewind()).toString());
+                        ByteBuffer pbuf = (ByteBuffer) UTF8.encode(payload).rewind();
+                        final int limit = pbuf.rewind().limit();
+                        Rfc822HeaderState.HttpResponse res = req.$res();
+                        ByteBuffer as = res.headerString(HttpHeaders.Content$2dType, MimeType.json.contentType)
+                                .headerString(HttpHeaders.Content$2dLength, String.valueOf(limit))
+                                .as(ByteBuffer.class);
+                        int needed = as.rewind().limit() + limit;
+
+                        cursor = (ByteBuffer) ((ByteBuffer) (cursor.capacity() >= needed ? cursor.clear().limit(needed) : ByteBuffer.allocateDirect(needed))).put(as).put(pbuf).rewind();
+
+                        key.interestOps(SelectionKey.OP_WRITE);
+                    } catch (Exception e) {
+                        key.cancel();
+                        e.printStackTrace();  //todo: verify for a purpose
+                    } finally {
+                    }
+                }
+            });
+            return;
+        }
+        int write = channel.write(cursor);
+        if (!cursor.hasRemaining()) {
+            /*Socket socket = channel.socket();
+            socket.getOutputStream().flush();
+            socket.close();*/
+            key.interestOps(SelectionKey.OP_READ).attach(null);
+        }
+
+    }
 }
