e250e89d508a4c92170b53b5bb2233f0a68d8311
Jim Northrup <northrup.james@gmail.com>
2013-01-01 08:03:08 -0800

    write a unit test exploring some of the date parsing hell that is If-Changed-Since

--- rxf-server/src/test/java/rxf/server/Rfc822HeaderStateTest.java
+++ rxf-server/src/test/java/rxf/server/Rfc822HeaderStateTest.java
@@ -1,14 +1,81 @@
 package rxf.server;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 import org.junit.Test;
 
 import java.nio.ByteBuffer;
+import java.util.Date;
 
 import static one.xio.HttpMethod.UTF8;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class Rfc822HeaderStateTest {
+	static Gson GSON1 = new GsonBuilder().setDateFormat(
+			"EEEE, dd-MMM-yy HH:mm:ss zzz").create();
+	static Gson GSON2 = new GsonBuilder().setDateFormat(
+			"EEE, dd MMM yyyy HH:mm:ss zzz").create();
+	static Gson GSON3 = new GsonBuilder().setDateFormat(
+			"EEE MMM d HH:mm:ss yyyy").create();
+	static Gson GSON4 = new GsonBuilder().setDateFormat(
+			"yyyy-MM-dd'T'HH:mm:ss Z").create();
+
+	@Test
+	public void testIfModifiedStuff() {
+		String[] x = {"'sun, 11-sep-01 08:49:37 EST'",// ; RFC 850, obsoleted by RFC 1036
+				"'Sun, 1 Nov 1994 08:49:37 GMT'",//  ; RFC 822, updated by RFC 1123
+				"'Sun November  1 08:49:37 1994'",//   ; ANSI C's asctime() format
+				"'2007-03-01T13:00:00 PST8PDT'",//iso ?
+				"'2007-03-01T13:00:00.000 UTC'",//iso ?
+				"'2007-02-8T13:17:00Z'",//iso ?
+		};
+		String[] check = {"Tue Sep 11 06:49:37 PDT 2001",
+				"Tue Nov 01 00:49:37 PST 1994", "Tue Nov 01 08:49:37 PST 1994",
+				"Thu Mar 01 13:00:00 PST 2007", "Thu Mar 01 05:00:00 PST 2007",
+				"Thu Feb 08 05:17:00 PST 2007"};
+		for (int i = 0; i < x.length; i++) {
+			String s = x[i];
+			System.err.println("testing: " + s);
+			Date date = bruteForceParseIfModifiedSinceDate(s);
+			System.err.println("product: " + date);
+			assertEquals(date.getTime(), new Date(check[i]).getTime());
+		}
+
+	}
+
+	private Date bruteForceParseIfModifiedSinceDate(String s) {
+		System.err.println("java date: " + s);
+		try {
+			Date date = GSON1.fromJson(s, Date.class);
+			System.err.println("success 1");
+			return date;
+		} catch (JsonSyntaxException e) {
+			try {
+				Date date = GSON2.fromJson(s, Date.class);
+				System.err.println("success 2");
+				return date;
+			} catch (JsonSyntaxException e1) {
+				try {
+					Date date = GSON3.fromJson(s, Date.class);
+					System.err.println("success 3");
+					return date;
+				} catch (JsonSyntaxException e2) {
+					try {
+						Date date = GSON4.fromJson(s, Date.class);
+						System.err.println("success ISO");
+						return date;
+					} catch (JsonSyntaxException e3) {
+						Date date = BlobAntiPatternObject.GSON.fromJson(s,
+								Date.class);
+						System.err.println("success ISO+ms --BAPO");
+						return date;
+					}
+				}
+			}
+		}
+	}
 
 	@Test
 	public void testAppendHeadersOne() {
