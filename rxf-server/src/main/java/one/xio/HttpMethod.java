f67c3d751b542f08d42a3d26c76b0aeaa4789e56
jim <northrup.james@gmail.com>
2013-01-14 15:56:52 -0800

    pre-static 0.9.1 edition 0.9.2 ?

---
+++ rxf-server/src/main/java/one/xio/HttpMethod.java
@@ -0,0 +1,11 @@
+package one.xio;
+
+/**
+ * See  http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html
+ * User: jim
+ * Date: May 6, 2009
+ * Time: 10:12:22 PM
+ */
+public enum HttpMethod {
+  GET, POST, PUT, HEAD, DELETE, TRACE, CONNECT, OPTIONS, HELP, VERSION, ;
+}
