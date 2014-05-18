205daa248f83bb08a7921a72160d88b89e4f7c37
Jim Northrup <northrup.james@gmail.com>
2014-05-18 15:54:04 -0700

    bring over Kouchdb initiator

---
+++ rxf-server/src/main/java/rxf/server/WebSocketFrameBuilder.java
@@ -0,0 +1,54 @@
+package rxf.server;
+
+import java.util.Random;
+
+public class WebSocketFrameBuilder {
+  private byte[] maskingKey = null;
+  private long payloadLength = 0;
+  private boolean isMasked = false;
+  private WebSocketHeader.OpCode opcode = WebSocketHeader.OpCode.text;
+  private boolean isFin = true;
+
+  public WebSocketFrameBuilder setMaskingKey(byte[] maskingKey) {
+
+    if (maskingKey == null) {
+      byte[] bytes = new byte[4];
+      new Random().nextBytes(bytes);
+      maskingKey = bytes;
+    }
+    this.maskingKey = maskingKey;
+
+    return this;
+  }
+
+  public WebSocketFrameBuilder setPayloadLength(long payloadLength) {
+    this.payloadLength = payloadLength;
+    return this;
+  }
+
+  /**
+   * @param isMasked
+   *            if true, also sets a random mask when no mask is set.
+   * @return this
+   */
+  public WebSocketFrameBuilder setIsMasked(boolean isMasked) {
+    this.isMasked = isMasked;
+    if (isMasked && maskingKey == null)
+      setMaskingKey(null);
+    return this;
+  }
+
+  public WebSocketFrameBuilder setOpcode(WebSocketHeader.OpCode opcode) {
+    this.opcode = opcode;
+    return this;
+  }
+
+  public WebSocketFrameBuilder setIsFin(boolean isFin) {
+    this.isFin = isFin;
+    return this;
+  }
+
+  public WebSocketHeader createWebSocketFrame() {
+    return new WebSocketHeader(maskingKey, payloadLength, isMasked, opcode, isFin);
+  }
+}
