99544abd973709bb8ff1b2cf35b14e628ec3754e
Colin Alworth <niloc132@gmail.com>
2014-06-23 14:53:04 -0700

    Generics stupidity

--- rxf-server/src/main/java/rxf/server/CookieRfc6265Util.java
+++ rxf-server/src/main/java/rxf/server/CookieRfc6265Util.java
@@ -2,6 +2,7 @@ package rxf.server;
 
 import java.io.Serializable;
 import java.net.URLDecoder;
+import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.util.ArrayList;
@@ -448,7 +449,7 @@ public enum CookieRfc6265Util {
             while (filt.hasRemaining() && ckey.hasRemaining() && filt.get() == ckey.get());
             if (!filt.hasRemaining() && !ckey.hasRemaining()) {
               ret =
-                  new Pair<Pair<Object, ByteBuffer>, Pair<?, ?>>(new Pair<>(ckey.reset(), ((ByteBuffer) buf.duplicate().reset().flip()
+                  new Pair<Pair<Buffer, ByteBuffer>, Pair<?, ?>>(new Pair<>(ckey.reset(), ((ByteBuffer) buf.duplicate().reset().flip()
                       .position(vBegin)).slice()), ret);
               break;
             }
