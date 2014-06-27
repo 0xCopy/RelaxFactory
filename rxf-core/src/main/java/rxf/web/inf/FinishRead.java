2e291290b5cce0c2f8c77af1a8751b194805ab75
Jim Northrup <northrup.james@gmail.com>
2014-06-27 14:03:37 -0700

    headerInterest in toString is now deepToString

--- rxf-core/src/main/java/rxf/web/inf/FinishRead.java
+++ rxf-core/src/main/java/rxf/web/inf/FinishRead.java
@@ -5,13 +5,12 @@ import one.xio.AsioVisitor;
 import java.nio.ByteBuffer;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.SelectionKey;
-import java.nio.channels.WritableByteChannel;
 
 /**
  * Created by jim on 5/21/14.
  */
-public class FinishWrite extends AsioVisitor.Impl {
-  public FinishWrite(ByteBuffer cursor, Runnable success) {
+public class FinishRead extends AsioVisitor.Impl {
+  public FinishRead(ByteBuffer cursor, Runnable success) {
     this.cursor = cursor;
 
     this.success = success;
@@ -21,12 +20,11 @@ public class FinishWrite extends AsioVisitor.Impl {
   private Runnable success;
 
   @Override
-  public void onWrite(SelectionKey key) throws Exception {
-    int write = ((WritableByteChannel) key.channel()).write(cursor);
-    if (write == -1)
+  public void onRead(SelectionKey key) throws Exception {
+    int read = ((ReadableByteChannel) key.channel()).read(cursor);
+    if (read == -1)
       key.cancel();
     if (!cursor.hasRemaining())
       success.run();
-
   }
 }
