96bae40527bd8627c674a92f160129076a337374
Jim Northrup <northrup.james@gmail.com>
2014-07-05 14:21:27 -0700

    apply static imports

--- rxf-couch/src/main/java/rxf/couch/DocFetchProxyImpl.java
+++ rxf-couch/src/main/java/rxf/couch/DocFetchProxyImpl.java
@@ -3,12 +3,10 @@ package rxf.couch;
 // import com.colinalworth.gwt.viola.web.server.mvp.ViolaServerApp;
 
 import com.google.gwt.safehtml.shared.UriUtils;
-import one.xio.HttpHeaders;
 import one.xio.HttpStatus;
 import rxf.core.Errors;
 import rxf.core.Rfc822HeaderState;
 import rxf.couch.driver.CouchMetaDriver;
-import rxf.couch.gen.CouchDriver;
 import rxf.rpc.RpcHelper;
 import rxf.shared.KeepMatcher;
 import rxf.shared.PreRead;
@@ -19,8 +17,13 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.util.regex.MatchResult;
 
+import static java.nio.channels.SelectionKey.OP_READ;
+import static java.nio.channels.SelectionKey.OP_WRITE;
+import static one.xio.HttpHeaders.Content$2dLength;
+import static one.xio.HttpHeaders.Content$2dType;
 import static rxf.core.Rfc822HeaderState.HttpRequest;
 import static rxf.core.Rfc822HeaderState.HttpResponse;
+import static rxf.couch.gen.CouchDriver.DocFetch;
 
 /**
  * adapted from viola HttpProxyImpl.
@@ -31,88 +34,72 @@ import static rxf.core.Rfc822HeaderState.HttpResponse;
 @KeepMatcher
 public abstract class DocFetchProxyImpl extends ContentRootImpl {
 
-  @Override
-  public void onRead(final SelectionKey outerKey) throws Exception {
+    @Override
+    public void onRead(final SelectionKey outerKey) throws Exception {
 
-    if (outerKey.attachment() instanceof Object[]) {
-      Object[] ar = (Object[]) outerKey.attachment();
-      for (Object o : ar) {
-        if (o instanceof Rfc822HeaderState) {
-          setReq((HttpRequest) o);
-          break;
-        }
-        if (o instanceof MatchResult) {
-          setMatchResults((MatchResult) o);
-          break;
-        }
-      }
-    }
-    if (null != getReq() && null != getMatchResults()) {
-      outerKey.interestOps(0);
-      final HttpRequest outerRequest = getReq();
-      final String path = outerRequest.path();
-
-      MatchResult matcher = getMatchResults();
-      String link = UriUtils.sanitizeUri(getPrefix() + matcher.group(1) + getSuffix());
-      if (link.endsWith("/")) {
-        link += "index.html";
-      }
-      // sets up the threadlocals for CouchMetaDriver calls
-      new CouchDriver.DocFetch().db("").docId(link).to().state().$res().addHeaderInterest(
-          HttpHeaders.Content$2dType);
-
-      RpcHelper.EXECUTOR_SERVICE.submit(new Runnable() {
-        // static calls happen in outer thread.
-        DbKeysBuilder dbKeysBuilder = DbKeysBuilder.get();
-        ActionBuilder actionBuilder = ActionBuilder.get();
-
-        public void run() {
-          DbKeysBuilder.currentKeys.set(dbKeysBuilder);
-          ActionBuilder.currentAction.set(actionBuilder);
-          try {
-            HttpResponse innerResponse = actionBuilder.state().$res();
-            // getReq() is never sent in any part to couchdb here.
-            ByteBuffer outerPayload = CouchMetaDriver.DocFetch.visit(dbKeysBuilder, actionBuilder);
-            HttpStatus innerStatus = innerResponse.statusEnum();
-            if (HttpStatus.$404 == innerStatus) {
-              Errors.$404(outerKey, path);
-              return;
+        if (outerKey.attachment() instanceof Object[]) {
+            Object[] ar = (Object[]) outerKey.attachment();
+            for (Object o : ar) {
+                if (o instanceof Rfc822HeaderState) {
+                    setReq((HttpRequest) o);
+                    break;
+                }
+                if (o instanceof MatchResult) {
+                    setMatchResults((MatchResult) o);
+                    break;
+                }
             }
-            HttpResponse outerResponse = outerRequest.$res();
-            outerKey.interestOps(SelectionKey.OP_WRITE).attach(
-                new FinishWrite(outerResponse.status(innerStatus).headerString(
-                    HttpHeaders.Content$2dType,
-                    innerResponse.headerString(HttpHeaders.Content$2dType)).headerString(
-                    HttpHeaders.Content$2dLength,
-                    innerResponse.headerString(HttpHeaders.Content$2dLength)).asByteBuffer(),
-                    outerPayload) {
-                  @Override
-                  public void onSuccess() {
-                    outerKey.interestOps(SelectionKey.OP_READ).attach(null);
-                  }
-                });
-          } catch (Exception e) {
-            Errors.$500(outerKey);
-            e.printStackTrace();
-          }
         }
-      });
-    } else {
-      Errors.$500(outerKey);
+        if (null != getReq() && null != getMatchResults()) {
+            outerKey.interestOps(0);
+            final HttpRequest outerRequest = getReq();
+            final String path = outerRequest.path();
+
+            MatchResult matcher = getMatchResults();
+            String link = UriUtils.sanitizeUri(getPrefix() + matcher.group(1) + getSuffix());
+            if (link.endsWith("/")) {
+                link += "index.html";
+            }
+            // sets up the threadlocals for CouchMetaDriver calls
+            new DocFetch().db("").docId(link).to().state().$res().addHeaderInterest(
+                    Content$2dType);
+
+            RpcHelper.EXECUTOR_SERVICE.submit(new Runnable() {
+                // static calls happen in outer thread.
+                DbKeysBuilder dbKeysBuilder = DbKeysBuilder.get();
+                ActionBuilder actionBuilder = ActionBuilder.get();
+
+                public void run() {
+                    DbKeysBuilder.currentKeys.set(dbKeysBuilder);
+                    ActionBuilder.currentAction.set(actionBuilder);
+                    try {
+                        HttpResponse innerResponse = actionBuilder.state().$res();
+                        // getReq() is never sent in any part to couchdb here.
+                        ByteBuffer outerPayload = CouchMetaDriver.DocFetch.visit(dbKeysBuilder, actionBuilder);
+                        HttpStatus innerStatus = innerResponse.statusEnum();
+                        if (HttpStatus.$404 == innerStatus) {
+                            Errors.$404(outerKey, path);
+                            return;
+                        }
+                        HttpResponse outerResponse = outerRequest.$res();
+                        outerKey.interestOps(OP_WRITE).attach(
+                                new FinishWrite(outerResponse.status(innerStatus).headerString(Content$2dType, innerResponse.headerString(Content$2dType)).headerString(Content$2dLength, innerResponse.headerString(Content$2dLength)).asByteBuffer(), outerPayload) { @Override public void onSuccess() { outerKey.interestOps(OP_READ).attach(null); } }); } catch (Exception e) { Errors.$500(outerKey); e.printStackTrace(); } } });
+        } else {
+            Errors.$500(outerKey);
+        }
     }
-  }
 
-  /**
-   * prepended to inner request
-   * 
-   * @return string
-   */
-  public abstract String getPrefix();
+    /**
+     * prepended to inner request
+     *
+     * @return string
+     */
+    public abstract String getPrefix();
 
-  /**
-   * appended to inner request
-   * 
-   * @return string
-   */
-  public abstract String getSuffix();
+    /**
+     * appended to inner request
+     *
+     * @return string
+     */
+    public abstract String getSuffix();
 }
