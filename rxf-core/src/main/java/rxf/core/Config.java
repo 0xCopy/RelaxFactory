f7c2ca3d28e911664de799451b6b05fb2a7a8452
Jim Northrup <northrup.james@gmail.com>
2014-07-20 10:26:57 -0700

    . in config supported

--- rxf-core/src/main/java/rxf/core/Config.java
+++ rxf-core/src/main/java/rxf/core/Config.java
@@ -3,7 +3,7 @@ package rxf.core;
 public class Config {
 
   public static String get(String rxf_var, String... defaultVal) {
-    String javapropname = "rxf." + rxf_var.toLowerCase().replaceAll("^rxf_", "").replace('_', '.');
+    String javapropname = "rxf." + rxf_var.toLowerCase().replaceAll("^rxf[_.]", "").replace('_', '.');
     String rxfenv = System.getenv(rxf_var);
     String var = null == rxfenv ? System.getProperty(javapropname) : rxfenv;
     var = null == var && defaultVal.length > 0 ? defaultVal[0] : var;
