3d902bbc0394c62a0614ba0546717cecfd4b25e9
Jim Northrup <northrup.james@gmail.com>
2014-08-08 14:55:10 -0700

    redir redux

--- rxf-core/src/main/java/rxf/core/Errors.java
+++ rxf-core/src/main/java/rxf/core/Errors.java
@@ -2,7 +2,6 @@ package rxf.core;
 
 import bbcursive.Cursive;
 import one.xio.AsioVisitor.Helper.F;
-import one.xio.AsioVisitor.Impl;
 import one.xio.HttpHeaders;
 import one.xio.HttpStatus;
 
@@ -10,9 +9,8 @@ import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 
 import static bbcursive.std.bb;
-import static java.nio.charset.StandardCharsets.UTF_8;
+import static bbcursive.std.str;
 import static one.xio.AsioVisitor.Helper.finishWrite;
-import static one.xio.AsioVisitor.Helper.write;
 
 public class Errors {
 
@@ -27,26 +25,22 @@ public class Errors {
 
   private static void redir(SelectionKey key, final String newUrl) {
     String message = "Resource moved to <a href='" + newUrl + "'>" + newUrl + "</a>";
-    final String html =
-        "<html><head><title>Resource Moved</title></head><body><div>" + message
-            + "</div><div><a href='/'>Back to home</a></div></body></html>";
-    key.attach(new Impl() {
-
-      public void onWrite(SelectionKey key) throws Exception {
-        ByteBuffer headers =
-            new Rfc822HeaderState().$res().status(HttpStatus.$303).headerString(
-                HttpHeaders.Content$2dType, "text/html").headerString(HttpHeaders.Location, newUrl)
-                .headerString(HttpHeaders.Content$2dLength, String.valueOf(html.length())).as(
-                    ByteBuffer.class);
-
-        write(key, headers);
-        write(key, UTF_8.encode(html));
-        key.selector().wakeup();
+    ByteBuffer bb =
+        bb("<html><head><title>Resource Moved</title></head><body><div>" + message
+            + "</div><div><a href='/'>Back to home</a></div></body></html>");
+    Rfc822HeaderState rfc822HeaderState = new Rfc822HeaderState().asResponse()//
+        .status(HttpStatus.$303)//
+        .headerString(HttpHeaders.Content$2dType, "text/html")//
+        .headerString(HttpHeaders.Location, newUrl)//
+        .headerString(HttpHeaders.Content$2dLength, str(bb.limit()))//
+    ;
+    finishWrite(key, new F() {
+      @Override
+      public void apply(SelectionKey key) throws Exception {
         key.interestOps(SelectionKey.OP_READ).attach(null);
       }
-    });
+    }, bb(rfc822HeaderState), bb);
     key.interestOps(SelectionKey.OP_WRITE);
-
   }
 
   public static void $400(SelectionKey key) {
